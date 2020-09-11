128644467e17f6d673d6ca6ccd6a30cec9809ed5
Stacy Daniel <stacydn23@gmail.com>
2020-09-11 14:54:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ function happyHolidaysTo(name){
 function happyCustomHolidayTo(holiday, name){
   return `Happy ${holiday}, ${name}!`
 }
-function holidayCountdown(holiday, days){
+function holidayCountdown(mothers day , 8){
   return `It's ${days} days until ${holidays}!`
 }
