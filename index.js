b42f0ce70e49e4c3254a09e9f3b603447a2f34f6
Stacy Daniel <stacydn23@gmail.com>
2020-09-11 14:38:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ function happyHolidays() {
 function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`
 }
+function happyCustomHolidayTo(Thanksgiving, name){
+  return `Happy ${thanksgiving},${name}!`
+}
