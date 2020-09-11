73a2f19591cf0319459100a323e892ff05cc454f
Stacy Daniel <stacydn23@gmail.com>
2020-09-11 12:55:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// write your code below!
+function happyHoliday(){
+  return "Happy holidays!"
+}
+function happyHolidaysTo(name){
+  return `Happy holidays,${name}!`
+}
+function happyCustomHolidayTo(holiday, name){
+  return `Happy ${holiday}`,${name}!`
+}
