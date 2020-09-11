c43bdcdd3b5cd8c50280f6859e501cc1394ed1f3
Stacy Daniel <stacydn23@gmail.com>
2020-09-11 14:04:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
 function happyHolidays() {
   return "Happy holidays!"
 }
-function happyHolidaysTo(Kevin){
-  return `Happy holidays,${You}!`
-}
-function happyCustomholidayTo(Thanksgiving, name){
-  return `Happy ${Thanksgiving}, $ {name}!`
-}
-function holidayCountdown(Thanksgiving, 10){
-  `It\'s ${10} days until ${Thanksgiving}!`
-}
